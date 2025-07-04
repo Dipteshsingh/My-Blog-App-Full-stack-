8547ed6a7e652260c0629c770677fe3763d43942
Dipteshsingh <diptesh.singh73@gmail.com>
2025-07-04 13:05:52 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -83,7 +83,7 @@ const loginUser = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production', // true in production, false in dev
-      sameSite: 'Lax',
+      sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
